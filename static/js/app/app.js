bebe0be0299dbdb47e95857a789c3674021192ea
Christian Hill <Christian.T.Hill@jpl.nasa.gov>
2016-10-25 11:40:35 -0700

    Angular app up and running

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -1,1 +1,3 @@
-console.log("hello")
+app = angular.module('cppcarpool', [
+    'app.controllers.main'
+]);
---
+++ static/js/app/main/mainCtrl.js
@@ -0,0 +1,6 @@
+var mainCtrl = angular.module('app.controllers.main', []);
+
+mainCtrl.controller('mainCtrl', ['$scope', function($scope) {
+    var vm = this;
+    console.log("MainCtrl");
+}]);
