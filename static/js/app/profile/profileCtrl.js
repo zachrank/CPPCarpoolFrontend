044112a8d49f4f6c31a5f8e4c1b8e9e6bbd0b29a
Christian Hill <Christian.T.Hill@jpl.nasa.gov>
2016-11-20 17:51:12 -0800

    Better mobile support

--- static/js/app/profile/profileCtrl.js
+++ static/js/app/profile/profileCtrl.js
@@ -1,6 +1,6 @@
 var profile = angular.module('app.profile', []);
 
-profile.controller('profileCtrl', ['$scope', '$http', '$routeParams', 'authFactory', function($scope, $http, $routeParams, authFactory) {
+profile.controller('profileCtrl', ['$scope', '$http', '$routeParams', 'authFactory', '$sce', function($scope, $http, $routeParams, authFactory, $sce) {
     var vm = this;
     vm.data = {};
     vm.loading = true;
@@ -12,6 +12,10 @@ profile.controller('profileCtrl', ['$scope', '$http', '$routeParams', 'authFacto
         'Reviews',
         'Contact'
     ];
+    // maps key usage is restricted to *.cppcarpool.com domains
+    var mapsKey = 'AIzaSyCMycxWDmmKHTDK4xTFzgrjTqarniV8LLw';
+
+
 
     $http({
         'url': '/api/user/' + $routeParams.user,
@@ -19,6 +23,8 @@ profile.controller('profileCtrl', ['$scope', '$http', '$routeParams', 'authFacto
     }).then(function(response) {
         vm.data = response.data;
         vm.data.stars = [2,2,2,1,0];
+        vm.mapsUrl = $sce.trustAsUrl('https://www.google.com/maps/embed/v1/place?key=' + mapsKey + '&q=' + (vm.data.city || 'Cal Poly Pomona'));
+
         vm.loading = false;
     }, function(response) {
         vm.error = true;
