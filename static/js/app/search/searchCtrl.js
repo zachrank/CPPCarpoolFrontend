a720fbe61f7e0fe79ceb95c3deed349e11a35c6b
Christian Hill <Christian.T.Hill@jpl.nasa.gov>
2016-11-25 13:39:51 -0800

    Started on search frontend

--- static/js/app/search/searchCtrl.js
+++ static/js/app/search/searchCtrl.js
@@ -2,4 +2,17 @@ var search = angular.module('app.search', []);
 
 search.controller('searchCtrl', ['$scope', function($scope) {
     var vm = this;
+    vm.results = [{
+        'cppusername': 'cthill',
+        'fullname': 'James Polk',
+        'stars': 5,
+        'reviews': [{}],
+        'distance': 13
+    }, {
+        'cppusername': 'cthill',
+        'fullname': 'Osama bin Laden',
+        'stars': 1,
+        'reviews': [{}, {}],
+        'distance': 20
+    }];
 }]);
