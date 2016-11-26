58118908e08a5e6c2bc243a33d05146526a43b01
Christian Hill <Christian.T.Hill@jpl.nasa.gov>
2016-11-25 17:06:29 -0800

    Progress on search page

--- static/js/app/search/searchCtrl.js
+++ static/js/app/search/searchCtrl.js
@@ -1,20 +1,25 @@
 var search = angular.module('app.search', []);
 
-search.controller('searchCtrl', ['$scope', function($scope) {
+search.controller('searchCtrl', ['$scope', '$http', function($scope, $http) {
     var vm = this;
-    vm.results = [{
-        'cppusername': 'cthill',
-        'fullname': 'James Polk',
-        'stars': 5,
-        'reviews': [{}],
-        'distance': 13,
-        'city': 'China Hills'
-    }, {
-        'cppusername': 'cthill',
-        'fullname': 'Osama bin Laden',
-        'stars': 1,
-        'reviews': [{}, {}],
-        'distance': 20,
-        'city': 'China Hills'
-    }];
+    vm.results = [];
+    vm.loading = true;
+    vm.error = false;
+
+    $http({
+        'url': '/api/findRides/',
+        'method': 'GET'
+    }).then(function(response){
+        vm.results = response.data.results;
+        for (var i = 0; i < vm.results.length; i++) {
+            var atIndex = vm.results[i].cppemail.indexOf('@');
+            vm.results[i].cppusername = vm.results[i].cppemail.slice(0, atIndex);
+        }
+
+        vm.loading = false;
+        vm.error = false;
+    }, function(response) {
+        vm.loading = false;
+        vm.error = true;
+    });
 }]);
